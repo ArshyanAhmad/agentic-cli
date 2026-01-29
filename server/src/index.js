52551cdaff8787f1f0d27765f464be7893f5b033
Arsh <arshahmad235@gmail.com>
2026-01-29 20:11:56 +0530

    better auth implemented in server

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,30 @@
 import express from "express";
-const app = express();
+import { fromNodeHeaders, toNodeHandler } from "better-auth/node";
+import { auth } from "../lib/auth,js";
+import cors from "cors";
 import "dotenv/config";
 
+const app = express();
+
+app.use(
+   cors({
+      origin: "http://localhost:3000", // Replace with your frontend's origin
+      methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+      credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+   }),
+);
+
 const PORT = process.env.PORT || 3005;
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
+app.use(express.json());
+
+app.get("/api/me", async (req, res) => {
+   const session = await auth.api.getSession({
+      headers: fromNodeHeaders(req.headers),
+   });
+   return res.json(session);
+});
 
 app.get("/health", (req, res) => {
    res.send("Running");
